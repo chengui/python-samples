318281c0621d93781295b5926c61b2025a436a19
Gui Chen <gui.chen@intel.com>
2013-07-19 05:32:57 -0400

    safecopy.py: a safe copy function in python
    Signed-off-by: Gui Chen <gui.chen@intel.com>

---
+++ safecopy.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python
+"""
+An safe copy for python, it can handle copy tree and file totally,
+and also provide an ignore patterns.
+"""
+import os
+import sys
+import shutil
+
+def safecopy(src, dst, symlinks=False, ignore_ptns=[]):
+    if symlinks and os.path.islink(src):
+        if os.path.isdir(dst):
+            dst = os.path.join(dst, os.path.basename(src))
+        linkto = os.readlink(src)
+        os.symlink(linkto, dst)
+    elif os.path.isdir(src):
+        src = src.rstrip('/')
+        if os.path.isdir(dst):
+            dst = os.path.join(dst, os.path.basename(src))
+
+        # check common prefix
+        if dst.startswith(src +'/'):
+            ignore_ptns += os.path.basename(src)
+
+        ignores = shutil.ignore_patterns(*ignore_ptns)
+        try:
+            shutil.copytree(src, dst, symlinks, ignores)
+        except OSError, IOError:
+            shutil.rmtree(dst, ignore_errors=True)
+            raise
+    else:
+        try:
+            shutil.copy2(src, dst)
+        except OSError, IOError:
+            raise
+
+    return
+
