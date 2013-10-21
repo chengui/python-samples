4906172b8be084f5e724fca75def847ec496414f
Gui Chen <gui.chen@intel.com>
2013-10-21 14:40:41 +0800

    add TeeHandler to log stdout/stderr in logging module
    this TeeHandler will log stdout/stderr to logging modlue,
    and will record all messages to a file
    
    Signed-off-by: Gui Chen <gui.chen@intel.com>

---
+++ teelog.py
@@ -0,0 +1,57 @@
+"""
+TeeHandler is supposed to log messages from stdout/stderr
+as well as logging module, and the log target will be both
+stdout/stderr and file, just like what 'tee' do in linux
+"""
+
+import sys
+import logging
+
+class TeeStream(object):
+    """ Tee-like stream that sync output to standard stream and file """
+    def __init__(self, stream, streamfd=0):
+        self.stream = stream
+        self.streamfd = streamfd
+        if self.streamfd == 1:
+            self.stdstream = sys.stdout
+            sys.stdout = self
+        if self.streamfd == 2:
+            self.stdstream = sys.stderr
+            sys.stderr = self
+
+    def __del__(self):
+        if self.streamfd == 1:
+            sys.stdout = self.stdstream
+        if self.streamfd == 2:
+            sys.stderr = self.stdstream
+
+    def write(self, data):
+        """ Write data to file and standard stream """
+        if self.streamfd in (1, 2):
+            self.stream.write(data)
+            self.stdstream.write(data)
+
+
+class TeeHandler(logging.FileHandler):
+    """ Handler that log output from stdout/stderr via TeeStream """
+    def __init__(self, filename, streamfd=0, **kwargs):
+        logging.FileHandler.__init__(self, filename, **kwargs)
+        self.stream = TeeStream(self.stream, streamfd)
+
+
+def test():
+    """ Test routine """
+    logger = logging.Logger('root', logging.DEBUG)
+    handler = TeeHandler('./log', 1, mode='w')
+    logger.addHandler(handler)
+
+    print >> sys.stderr, "hello, stderr"
+    print >> sys.stdout, "hello, stdout"
+
+    logger.debug("debug")
+    logger.info("info")
+    logger.warning("warning")
+    logger.error("error")
+
+if __name__ == "__main__":
+    test()
