fc7bb7f38d4eced37925cf804f02452bd4de06de
Gui Chen <gui.g.chen@gmail.com>
2017-08-18 10:29:10 +0800

    Add Tree class with defaultdict
    Signed-off-by: Gui Chen <gui.g.chen@gmail.com>

--- tree.py
+++ tree.py
@@ -24,4 +24,33 @@ taxonomy['Plantae']['Solanales']['Convolvulaceae']['Ipomoea']['sweet potato']
 
 print_tree(taxonomy)
 
+print('\n' + '-' * 80 + '\n')
 
+
+class Tree(object):
+    def __new__(cls, *args, **kwargs):
+        return defaultdict(cls)
+
+    def __call__(self):
+        return Tree()
+
+    def __str__(self):
+        def _str(tree, buf, prefix):
+            for k, v in tree.iteritems():
+                buf.append(prefix + '+- ' + k)
+                if v:
+                    _str(tree[k], buf, '|   ' + prefix)
+        buf = []
+        _str(self, buf, '')
+        return '\n'.join(buf)
+
+Taxonomy = Tree()
+Taxonomy['Animalia']['Chordata']['Mammalia']['Carnivora']['Felidae']['Felis']['cat']
+Taxonomy['Animalia']['Chordata']['Mammalia']['Carnivora']['Felidae']['Panthera']['lion']
+Taxonomy['Animalia']['Chordata']['Mammalia']['Carnivora']['Canidae']['Canis']['dog']
+Taxonomy['Animalia']['Chordata']['Mammalia']['Carnivora']['Canidae']['Canis']['coyote']
+Taxonomy['Plantae']['Solanales']['Solanaceae']['Solanum']['tomato']
+Taxonomy['Plantae']['Solanales']['Solanaceae']['Solanum']['potato']
+Taxonomy['Plantae']['Solanales']['Convolvulaceae']['Ipomoea']['sweet potato']
+
+print_tree(Taxonomy)
